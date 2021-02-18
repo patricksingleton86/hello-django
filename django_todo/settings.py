9053ea15e2b9ca109bb2f0b1124777a32378da40
patricksingleton86 <patricksingleton95@gmail.com>
2021-02-18 15:23:03 +0000

    Set up environment variables and automatic deployment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '2ibcjubyt3x9b9l$tafcny)xcbicq7#)36=%u4%p22j_xva!)f')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
